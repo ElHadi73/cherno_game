b8acbc8670be8a6329c444e246b497b5e086264f
root <root@you.you>
2022-01-23 13:37:10 -0500

    stoped in episod 65

---
+++ com/thecherno/rain/entity/mob/Mob.java
@@ -0,0 +1,45 @@
+package com.thecherno.rain.entity.mob;
+
+import com.thecherno.rain.entity.Entity;
+import com.thecherno.rain.graphics.Sprite;
+
+public abstract class Mob extends Entity {
+
+	protected Sprite sprite;
+	protected int dir = 0;
+	protected boolean moving = false;
+
+	public void move(int xa, int ya) {
+		if(xa != 0 && ya != 0) {
+			move(xa, 0);
+			move(0, ya);
+			return;
+		}
+
+		if(xa > 0) dir = 1;
+		if(xa < 0) dir = 3;
+		if(ya > 0) dir = 2;
+		if(ya < 0) dir = 0;
+
+		if(!collision(xa, ya)) {
+			x += xa;
+			y += ya;
+		}
+	}
+
+	public void update() {
+	}
+
+	public void render() {
+	}
+
+	private boolean collision(int xa, int ya) {
+		boolean solid = false;
+		for (int c = 0; c < 4; c++) {
+			int xt = ((x + xa) + c % 2 * 14 - 8) / 16;
+			int yt = ((y + ya) + c / 2 * 12 + 3) / 16;
+			if(level.getTile((x + xa) / 16, (y + ya) / 16).solid()) solid = true;
+		}
+		return solid;
+	}
+}
