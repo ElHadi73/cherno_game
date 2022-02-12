37249e8227afc3a7c7b0af9d2ff48bd02e96035f
root <root@you.you>
2022-02-12 04:38:14 -0500

    1P: all one side cars can cross att time

--- com/thecherno/rain/Game.java
+++ com/thecherno/rain/Game.java
@@ -107,7 +107,7 @@ public class Game extends Canvas implements Runnable {
 			//create car
 			System.out.println(cars_list[0].size()+"  |  "+cars_list[1].size()+"  |  "+cars_list[2].size()+"  |  "+cars_list[3].size());
 			enter = random.nextInt(4);
-			if(random.nextInt(10) == 0 && free_slut(enter)){
+			if(random.nextInt(300) == 0 && free_slut(enter)){
 				car = new Car(cars_list,enter);
 				cars_list[enter].add(car);
 				car_thread = new Thread(car,String.valueOf(car_n));
